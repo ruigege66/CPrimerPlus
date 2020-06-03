90b6b56b2453783be8946b77b1d85c919cd2004b
dongqianrui <1215217867@qq.com>
2020-06-04 00:21:30 +0800

    Create D1_firstProgramme.c

---
+++ D1_firstProgramme.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void builder(void);
+
+int main(void){
+
+    printf("I will summon the buttler function.\n");
+
+    buttler();
+
+    printf("haoba");
+
+   
+
+    return 0;
+
+}
+
+void buffer(void){
+
+    printf("buffer function");
+
+}
