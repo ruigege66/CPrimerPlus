c17c9104a667e9a7b59155e60c48e791407cbb2d
dongqianrui <1215217867@qq.com>
2020-07-08 00:01:56 +0800

    Create D13_typesize.c

---
+++ D13_typesize.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int D13_typesize(void) {
+	//C99为类型提供了%zd转换说明
+	printf("Type int has a size of %zd bytes.\n", sizeof(int));
+	printf("Type char has a size of %zd bytes.\n", sizeof(char));
+	printf("Type long has a size of %zd bytes.\n", sizeof(long));
+	printf("Type long long has a size of %zd bytes.\n", sizeof(long long));
+	printf("Type double has a size of %zd bytes.\n", sizeof(double));
+	printf("Type long double has a size of %zd bytes.\n", sizeof(long double));
+
+
+	return 0;
+}
