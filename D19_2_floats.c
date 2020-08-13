aa2738ba8be8e4883c662ffd15dd0a6493c1b7cc
dongqianrui <1215217867@qq.com>
2020-08-13 23:54:41 +0800

    Create D19_2_floats.c

---
+++ D19_2_floats.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int D19_2_floats(void) {
+	const double RENT = 3852.99;   //const 变量
+	printf("*%f*\n", RENT);
+	printf("*%e*\n", RENT);
+	printf("*%4.2f*\n", RENT);
+	printf("*%3.1f*\n", RENT);
+	printf("*%10.3f*\n", RENT);
+	printf("*%10.3E*\n", RENT);
+	printf("*%+4.2f*\n", RENT);
+	printf("*%010.2f*\n", RENT);
+
+	return 0;
+}
